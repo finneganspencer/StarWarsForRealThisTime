696f7db0d04424735a6d4dfbb1a4a837784e866e
cpalm9 <letthewookiewin@Chriss-MacBook-Pro.local>
2016-12-06 21:28:06 -0700

    DAL

---
+++ StarWarsWebsite/DAL/StarWarsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using StarWarsWebsite.Models;
+
+namespace StarWarsWebsite.DAL
+{
+    public class StarWarsContext : DbContext
+    {
+        public StarWarsContext() : base("StarWarsContext")
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Questions> Question { get; set; }
+        public DbSet<Planet> Planets { get; set; }
+    }
+}
