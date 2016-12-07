52a1512630caf6d1e1956c8125a33c9994b0997b
cpalm9 <letthewookiewin@Chriss-MBP.app.byu.edu>
2016-12-07 16:40:35 -0700

    login

---
+++ StarWarsWebsite/Controllers/UsersController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StarWarsWebsite.DAL;
+using StarWarsWebsite.Models;
+
+namespace StarWarsWebsite.Controllers
+{
+    public static class GlobalVariables
+    {
+        public static String currentUser = "New User";
+        public static int currentUserID = 0;
+        public static String currentUserName = "New User";
+    }
+    public class UsersController : Controller
+    {
+        private StarWarsContext db = new StarWarsContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,UserEmail,Password,firstName,lastName")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                StarWarsWebsite.Controllers.GlobalVariables.currentUser = user.UserEmail;
+                StarWarsWebsite.Controllers.GlobalVariables.currentUserName = user.firstName;
+                StarWarsWebsite.Controllers.GlobalVariables.currentUserID = user.UserID;
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,UserEmail,Password,firstName,lastName")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection form, bool rememberMe = false)
+        {
+            String email = form["Email"].ToString();
+            String password = form["Password"].ToString();
+
+            User user = db.Users.SingleOrDefault(x => x.UserEmail == email);
+
+            if (user != null)
+            {
+                if (user.Password == password)
+                {
+                    StarWarsWebsite.Controllers.GlobalVariables.currentUser = email;
+                    StarWarsWebsite.Controllers.GlobalVariables.currentUserName = user.firstName;
+                    StarWarsWebsite.Controllers.GlobalVariables.currentUserID = user.UserID;
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return RedirectToAction("About", "Home");
+            }
+
+            return View();
+        }
+    }
+}
--- StarWarsWebsite/Models/User.cs
+++ StarWarsWebsite/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,9 +13,23 @@ namespace StarWarsWebsite.Models
     {
         [Key]
         public int UserID { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        [EmailAddress]
         public string UserEmail { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
         public string firstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
         public string lastName { get; set; }
     }
 }
